b36887e7f411e9ae878d8b508146b4b5af537312
LucianoGalvao <luciano_sdmf@hotmail.com>
2020-06-05 01:04:38 -0300

    Template base para adicionar, criado

---
+++ resources/views/contacts/create.blade.php
@@ -0,0 +1,52 @@
+@extends('base')
+
+@section('main')
+<div class="row">
+ <div class="col-sm-8 offset-sm-2">
+    <h1 class="display-3">Adicione um contato</h1>
+  <div>
+    @if ($errors->any())
+      <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+              <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+      </div><br />
+    @endif
+      <form method="post" action="{{ route('contacts.store') }}">
+          @csrf
+          <div class="form-group">    
+              <label for="primeiro_nome">Primeiro nome:</label>
+              <input type="text" class="form-control" name="primeiro_nome"/>
+          </div>
+          <div class="form-group">
+              <label for="sobrenome">Sobrenome:</label>
+              <input type="text" class="form-control" name="sobrenome"/>
+          </div>
+          <div class="form-group">
+              <label for="nascimento">Data de Nascimento:</label>
+              <input type="date" class="form-control" name="nascimento"/>
+          </div>
+          <div class="form-group">
+              <label for="telefone">Telefone:</label>
+              <input type="text" class="form-control" name="telefone"/>
+          </div>
+          <div class="form-group">
+              <label for="endereco">Endereço:</label>
+              <input type="text" class="form-control" name="endereco"/>
+          </div>
+          <div class="form-group">
+              <label for="cidade">Cidade:</label>
+              <input type="text" class="form-control" name="cidade"/>
+          </div>      
+          <div class="form-group">
+            <label for="estado">Estado:</label>
+            <input type="text" class="form-control" name="estado">
+          </div>                   
+          <button type="submit" class="btn btn-primary-outline">Adicionar contato</button>
+      </form>
+  </div>
+</div>
+</div>
+@endsection
